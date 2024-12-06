23042363cad62de6059a0dab10e37fa5819c07ec
PineappleMama <125321926+PineappleTech1213@users.noreply.github.com>
2024-12-06 00:29:06 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow () {
+    const win = new BrowserWindow({
+        width: 1400,
+        height: 900,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+
+    win.loadFile('index.html')
+    // Uncomment next line to remove menu bar
+    // win.setMenuBarVisibility(false)
+}
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+}) 
